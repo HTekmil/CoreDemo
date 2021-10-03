6c861165a8fab997e2ff6c3bef0b267e354ee3fe
Hüseyin Tekmil <67290385+HTekmil@users.noreply.github.com>
2021-10-04 02:14:30 +0300

    37. Ders Sonu

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı ve Soyadı boş bırakılamaz!");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş bırakılamaz!");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş bırakılamaz!");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Adınız ve Soyadınız 2 harften küçük olamaz!");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Adınız ve Soyadınız 50 harften büyük olamaz!");
+
+        }
+    }
+}
