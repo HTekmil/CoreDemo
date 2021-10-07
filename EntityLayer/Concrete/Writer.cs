71a8e47834856f92ae489adaa47e4a5bba0885db
Hüseyin Tekmil <67290385+HTekmil@users.noreply.github.com>
2021-10-07 23:20:48 +0300

    7 Ekim 39.Ders

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -22,6 +22,8 @@ namespace EntityLayer.Concrete
 
         public string WriterPassword { get; set; }
 
+        public string WriterCity { get; set; }
+
         public bool WriterStatus { get; set; }
 
         public List<Blog> Blogs { get; set; }
