b380b17badc671143871832a9d69fb0111432fd6
Hüseyin Tekmil <67290385+HTekmil@users.noreply.github.com>
2022-04-05 23:27:18 +0300

    5 Nisan

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responceMessage = await httpClient.GetAsync("https://localhost:44359/api/Default");
+            var jsonString = await responceMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+        }
+    }
+    public class Class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
