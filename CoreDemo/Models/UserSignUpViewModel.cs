9dc0a5e3ff59b3abef9120964468808f739e3e1e
Hüseyin Tekmil <67290385+HTekmil@users.noreply.github.com>
2022-05-03 19:09:08 +0300

    3 Mayıs

---
+++ CoreDemo/Models/UserSignUpViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soad")]
+        [Required(ErrorMessage ="Lütfen Ad Soad giriniz!")]
+        public string NameSurname { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen şifre giriniz!")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar")]
+        [Compare("Password",ErrorMessage ="Şifre eşleşmedi!")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Mail Adresi")]
+        [Required(ErrorMessage = "Lütfen mail giriniz!")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Lütfen Kullanıcı adı giriniz!")]
+        public string UserName { get; set; }
+    }
+}
